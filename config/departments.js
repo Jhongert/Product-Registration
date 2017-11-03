8277323d35ded4f5151827a3571fd279e389c948
Jhongert Fuertes <jfuertes@Jhongerts-MacBook-Pro.local>
2017-11-02 22:26:56 -0400

    Fix department list

--- config/departments.js
+++ config/departments.js
@@ -1,7 +1,7 @@
 module.exports = {
-	0: {"value":"books", "text":"Books"},
-	1: {"value":"electronic", "text":"Electronic"},
-	2: {"value":"movies", "text":"Movies"},
-	3: {"value":"home", "text":"Home"},
-	4: {"value":"beauty", "text":"Beauty"}
+	0: {"value":"Books", "text":"Books"},
+	1: {"value":"Electronic", "text":"Electronic"},
+	2: {"value":"Movies", "text":"Movies"},
+	3: {"value":"Home", "text":"Home"},
+	4: {"value":"Beauty", "text":"Beauty"}
 }
