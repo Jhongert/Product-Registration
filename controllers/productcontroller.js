f50ff2ab163114a32aac64f67e32d1d6a90b37e9
Jhongert Fuertes <jfuertes@Jhongerts-MacBook-Pro.local>
2017-11-02 22:56:37 -0400

    Fixing bugs

--- controllers/productcontroller.js
+++ controllers/productcontroller.js
@@ -1,19 +1,23 @@
 // Dependencies
 var express = require('express');
 var db = require('../models');
+// Requeri the list of departments from departments.js
+// This list will fill the select element in the view
 var departments = require('../config/departments');
 
-
+// Find all products and render list view
 exports.list = function(req, res){
 	db.product.findAll().then(function(data){
 		res.render("list", { products: data });
 	});
 }
 
+// Render view to create a new product
 exports.create = function(req, res){
 	res.render("create", { department: departments});
 }
 
+// Save the a new product in database and redirect to create
 exports.store = function(req, res){
 	db.product.create({
 		name: req.body.name,
@@ -27,6 +31,7 @@ exports.store = function(req, res){
 	});
 }
 
+// Delete a product from database and return ok
 exports.delete = function(req, res){
 	db.product.destroy({
 		where: 
@@ -37,6 +42,7 @@ exports.delete = function(req, res){
 	);
 }
 
+// Find a product by id and render the view to edit it
 exports.edit = function(req, res){
 	db.product.findById(req.params.id)
 		.then(function(data){
@@ -51,7 +57,7 @@ exports.edit = function(req, res){
 	});
 }
 
-
+// Update a product
 exports.update = function(req, res){
 	console.log(req.body.active);
 	db.product.update({
@@ -68,32 +74,3 @@ exports.update = function(req, res){
 	});
 	
 }
-
-
-// router.post('/burgers/insert', function(req, res){
-// 	db.Burger.create({
-// 		burger_name: req.body.burger_name
-// 	}).then(function(){
-// 		res.redirect('/');
-// 	});
-// });
-
-// router.post('/customer/insert', function(req, res){
-// 	db.Customer.create({
-// 		name: req.body.name,
-// 		BurgerId: req.body.burgerId
-// 	}).then(function(){
-// 		db.Burger.update({
-// 		devoured: true
-// 	}, {
-// 		where: {
-// 			id: req.body.burgerId
-// 		}
-// 	}).then(function(){
-// 		res.redirect('/');
-// 	});
-// 	})
-	
-// });
-
-// Export router
